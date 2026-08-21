c7186acdb1d40713329a64f3b99509539ad724c3
yatani <yatani1995@gmail.com>
2026-08-21 20:17:58 +0900

    ０徐算追加対応

--- calculator/ErrorHandler.java
+++ calculator/ErrorHandler.java
@@ -2,6 +2,5 @@ package calculator;
 public class ErrorHandler {
     public void handle(Exception e){
         System.err.println("Error occurred: " + e.getMessage());
-        e.printStackTrace();
     }
 }
